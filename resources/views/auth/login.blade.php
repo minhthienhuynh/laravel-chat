36a8c6380c22dd20203e790089ac24557e367ed4
thesea2201 <thebienpronguyen2201@gmail.com>
2023-06-11 13:15:13 +0700

    add registration url to the login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,6 +43,12 @@
                     {{ __('Log in') }}
                 </x-button>
             </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                    {{ __('Don\'t have an account? Sign up ') }}
+                </a>
+            </div>
         </form>
     </x-authentication-card>
 </x-guest-layout>
